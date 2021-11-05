6a98fc74316ca58d11abaa7904d0f3ddc7c58d4b
anthonp <anthony.a.picciano@gmail.com>
2021-11-05 02:28:14 -0500

    Loop bug fixed.

--- wordlist-building/date-generator.py
+++ wordlist-building/date-generator.py
@@ -1,11 +1,8 @@
 # Generate all different dates from 1899-2030
-# By Anthony P. Created 11/05/21 
+# By Anthony P. Created 11/05/21
 
 file = open("dates.txt", "w")
 for date in range(1899, 2031):
-        while date < 2031:
-                file.write(str(date))
-                file.write('\n')
+        file.write(str(date))
+        file.write('\n')
 file.close()
-
-
